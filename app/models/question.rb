803db288d9573e8f0f467ca5ad4ab2812fd84ec4
Vitaly Miroshkin <sysop14@gmail.com>
2021-04-22 13:34:01 +0300

    Add user and questions models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,30 @@
 class Question < ApplicationRecord
+  belongs_to :user
+
+  validates :text, :user, presence: true
+
+  # демонстрация жизненного циксла объекта
+  # навесили на все популярные коллбэки свои методы
+  before_validation :before_validation
+  after_validation :after_validation
+  before_save :before_save
+  before_create :before_create
+  after_save :after_save
+  after_create :after_create
+
+  before_update :before_update
+  after_update :after_update
+
+  before_destroy :before_destroy
+  after_destroy :after_destroy
+
+  private
+  # динамическая генерация методов
+  ['validation', 'save', 'create', 'update', 'destroy'].each do |action|
+    ['before', 'after'].each do |time|
+      define_method("#{time}_#{action}") do
+        puts "********> #{time} #{action}"
+      end
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,44 @@
+require 'openssl'
+
 class User < ApplicationRecord
+  # параметры работы модуля шифрования паролей
+  ITERATIONS = 20_000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  attr_accessor :password
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+  validates :password, confirmation: true, presence: true
+
+  before_save :encrypt_password
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email) # находим кандидата по email
+
+    # сравнивается password_hash
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
+
+  private
+
+  def encrypt_password
+    if self.password.present?
+      # создаём т.н. "соль" - рандомная строка усложняющая задачу хакерам
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      # создаём хэш пароля - длинная уникальная строка, из которой невозможно восстановить пароль
+      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+    end
+  end
 end
