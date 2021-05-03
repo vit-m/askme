35d58425c616a27ca52b5992923c62d45f5232fc
Vitaly Miroshkin <sysop14@gmail.com>
2021-05-03 21:28:37 +0300

    Fix plural form

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   validates :text, presence: true, length: { maximum: 255 }
+
+  scope :answered, -> { where.not(answer: nil) }
+  scope :unanswered, -> { where(answer: nil) }
 end
